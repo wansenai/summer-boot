0117e42ee871dbc263fa3444c7d75a5c8ac310ef
James Zow <jameszow@163.com>
2022-06-08 20:19:13 +0800

    add web3 web2 module

--- summer-boot/src/web2/server/server.rs
+++ summer-boot/src/web2/server/server.rs
@@ -2,10 +2,14 @@ use std::{
     cmp, io,
     net,
     sync::{Arc, Mutex},
-    time::Duration,
+    time::Duration, pin::Pin,
 };
 
+use std::net::{SocketAddr, TcpListener as StdTcpListener};
 use socket2::{Domain, Protocol, Socket, Type};
+
+use tokio::net::TcpListener;
+use tokio::time::Sleep;
 
 struct Config {
     host: Option<String>,
@@ -16,6 +20,12 @@ struct Config {
 pub struct SummerApplication {
     // config: Arc<Mutex<Config>>,
     pub backlog: u32,
+    pub addr: SocketAddr,
+    pub listener: TcpListener,
+    pub sleep_on_errors: bool,
+    pub tcp_keepalive_timeout: Option<Duration>,
+    pub tcp_nodelay: bool,
+    pub timeout: Option<Pin<Box<Sleep>>>,
 }
 
 impl SummerApplication {
@@ -23,7 +33,7 @@ impl SummerApplication {
         let sockets = self.bind(address).unwrap();
 
         for lst in sockets {
-            // self = self.listen(lst).unwrap();
+            self = self.listen(lst).unwrap();
         }
 
         Ok(self)
@@ -45,12 +55,61 @@ impl SummerApplication {
         }
 
         if success {
+            println!("success");
             Ok(sockets)
         } else if let Some(e) = error.take() {
             Err(e)
         } else {
             Err(io::Error::new(io::ErrorKind::Other, "无法绑定地址"))
         }
+    }
+
+    pub fn listen(mut self, lst: net::TcpListener) -> io::Result<Self> {
+        Ok(self)
+    }
+
+    pub fn new(addr: &SocketAddr) -> Self{
+        let std_listener = StdTcpListener::bind(addr).unwrap();
+
+        SummerApplication::from_std(std_listener)
+    }
+
+    pub fn from_std(std_listener: StdTcpListener) -> Self{
+        std_listener
+            .set_nonblocking(true).unwrap();
+        let listener = TcpListener::from_std(std_listener).unwrap();
+        SummerApplication::from_listener(listener)
+    }
+
+    pub fn bind_two(addr: &SocketAddr) -> Self{
+        SummerApplication::new(addr)
+    }
+
+    pub fn from_listener(listener: TcpListener) -> Self{
+        let addr = listener.local_addr().unwrap();
+        SummerApplication {
+            backlog: 5,
+            listener,
+            addr,
+            sleep_on_errors: true,
+            tcp_keepalive_timeout: None,
+            tcp_nodelay: false,
+            timeout: None,
+        }
+    }
+
+    pub fn local_addr(&self) -> SocketAddr {
+        self.addr
+    }
+
+    pub fn set_keepalive(&mut self, keepalive: Option<Duration>) -> &mut Self {
+        self.tcp_keepalive_timeout = keepalive;
+        self
+    }
+
+    pub fn set_nodelay(&mut self, enabled: bool) -> &mut Self {
+        self.tcp_nodelay = enabled;
+        self
     }
 }
 
@@ -65,4 +124,3 @@ fn create_tcp_listener(address: net::SocketAddr, backlog: u32) -> io::Result<net
     socket.listen(backlog).unwrap();
     Ok(net::TcpListener::from(socket))
 }
-
