a6150f2369826d4c166acdc0b3241a41d761fb25
yangshiqu <635408602@qq.com>
2022-06-13 21:23:16 +0800

    提供简单的tokio运行时，以及其宏

--- summer-boot-tests/src/main.rs
+++ summer-boot-tests/src/main.rs
@@ -1,6 +1,6 @@
 use summer_boot::web2::server::server;
     
-    #[tokio::main]
+    #[summer_boot::main]
     async fn main() {
         let addrs = "127.0.0.1:8081".parse().unwrap();
         server::SummerApplication::bind_two(&addrs);
--- summer-boot/src/lib.rs
+++ summer-boot/src/lib.rs
@@ -1,1 +1,13 @@
 pub mod web2;
+pub mod rt;
+
+/// 建立过程宏与summer boot的关联
+macro_rules! codegen_reexport {
+    ($name:ident) => {
+        #[cfg(feature = "macros")]
+        #[cfg_attr(docsrs, doc(cfg(feature = "macros")))]
+        pub use summer_boot_codegen::$name;
+    };
+}
+
+codegen_reexport!(main);
